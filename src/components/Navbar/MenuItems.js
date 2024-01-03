9c230c9ba2e420d474fd42e504595e6023e11a47
sdivyanshu90 <divyanshu74.80@gmail.com>
2024-01-03 17:06:48 +0530

    Final Commit

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,27 +1,28 @@
 export const MenuItems = [
-  {
-    title: "Home",
-    url: "#",
-    cName: "nav-links",
-  },
-  {
-    title: "Services",
-    url: "#",
-    cName: "nav-links",
-  },
-  {
-    title: "Products",
-    url: "#",
-    cName: "nav-links",
-  },
-  {
-    title: "Contact Us",
-    url: "#",
-    cName: "nav-links",
-  },
-  {
-    title: "About",
-    url: "#",
-    cName: "nav-links",
-  },
-];
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+]
