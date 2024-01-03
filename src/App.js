843214d4c39437a904d173e3f6090146eb585987
sdivyanshu90 <divyanshu74.80@gmail.com>
2024-01-03 14:24:52 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import "./Navbar.css";
 import { MenuItems } from "./MenuItems.js";
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo">React</h1>
+        <h1 className="navbar-logo">
+          React<i className="fab fa-react"></i>
+        </h1>
         <div className="menu-icon"></div>
         <ul>
           {MenuItems.map((item, index) => {
